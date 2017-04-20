974d2a4d74c399a0e17183f8f8696f7438059a0e
Taylor Brazelton <tbrazelton@smartfile.com>
2017-04-20 17:37:24 -0400

    Updated version to 0.2.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tdoc',
-    version='0.1',
+    version='0.2',
     description='Python To Markdown Documentation Tool',
     author='Taylor Brazelton',
     author_email='ryanb58@live.com',
